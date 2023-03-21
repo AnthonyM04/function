1ebe9c03d982f467d6a07524d39cad95d2f5307b
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-03-21 16:01:35 -0400

    :3

---
+++ function.js
@@ -0,0 +1,25 @@
+// function decleration
+function message(firstName) {
+    console.log('Hello ' + firstName)
+}
+
+function getMeow() {
+    console.log('Meow!')
+}
+
+function getSumTotal(firstNumber, secondNumber) {
+    const result = firstNumber + secondNumber
+    console.log(result)
+}
+
+//invoke the function
+getSumTotal(2,5);
+getSumTotal(2900,16000000)
+getSumTotal("sym","phony")
+getSumTotal("Back","Forward")
+getSumTotal("Forward","Back")
+message("Jarvis")
+message("cat, who are you?")
+// getMeow()
+// getMeow()
+// getMeow()
